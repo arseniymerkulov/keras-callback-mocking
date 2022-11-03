d3c26ee4dafe24a9e98f0b062725f71b58c0dd8e
Arseniy Merkulov <arseniy.merkulov@gmail.com>
2022-11-03 12:29:46 +0300

    init

---
+++ callback_mocker.py
@@ -0,0 +1,102 @@
+from capturemock import RECORD, REPLAY
+import inspect
+import json
+
+
+class CallbackMocker:
+    def __init__(self):
+        self.save_path = 'capturemock/callbacks.json'
+        self.callbacks = []
+
+    def add_callback_signature(self, callback_type, args, kwargs, callback_code, callback_output_name):
+        # take out 'self' argument
+        args = args[1:]
+
+        # take out decorators and method definition and return lines
+        lines = callback_code.split('\n')
+
+        definition_line = list(filter(lambda x: 'def ' in x, lines))[0]
+        arg_names = definition_line.split('(', 1)[1].replace('):', '').replace(' ', '')
+        arg_names = arg_names.split(',')
+        arg_names = [arg_name.split('=')[0] for arg_name in arg_names if arg_name != 'self']
+
+        return_lines = list(filter(lambda x: 'return ' in x, lines))
+
+        for return_line in return_lines:
+            i = lines.index(return_line)
+            lines[i] = lines[i].replace('return ', f'{callback_output_name} = ')
+
+        lines = [line + '\n' for line in lines if '@' not in line and 'def ' not in line]
+
+        indent = lines[0].split(' ')[0] + ' '
+        lines = [line.replace(indent, '') for line in lines]
+
+        callback_code = ''.join(lines)
+
+        self.callbacks.append({
+            'callback_type': callback_type,
+            'arg_names': arg_names,
+            'args': args,
+            'kwargs': kwargs,
+            'callback_code': callback_code
+        })
+
+    def save_callback_signatures(self):
+        with open(self.save_path, 'w') as file:
+            file.write(json.dumps(self.callbacks))
+
+    def eval_callback_signatures(self, callback_output_name, callback_output):
+        with open(self.save_path, 'r') as file:
+            callbacks = json.loads(file.read())
+
+            for callback in callbacks:
+                CallbackMocker.eval_callback_signature(callback, callback_output_name, callback_output)
+
+    @staticmethod
+    def eval_callback_signature(callback, callback_output_name, callback_output):
+        arguments = {}
+
+        args = callback['args']
+        kwargs = list(callback['kwargs'].values())
+
+        for i, key in enumerate(callback['arg_names']):
+            if i < len(args):
+                arguments[key] = args[i]
+            else:
+                arguments[key] = kwargs[i - len(args)]
+
+        arguments[callback_output_name] = callback_output
+        local = {}
+
+        exec(callback['callback_code'], arguments, local)
+        callback_output.append(local[callback_output_name])
+
+
+def callbackmock(callback_mocker: CallbackMocker, callback_output_name):
+    def decorator(callback):
+        def wrapper(*args, **kwargs):
+            callback_mocker.add_callback_signature(callback.__name__,
+                                                   args,
+                                                   kwargs,
+                                                   inspect.getsource(callback),
+                                                   callback_output_name)
+            callback(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def callbackmocker(callback_mocker: CallbackMocker, callback_output_name, callback_output, mode):
+    def decorator(callback):
+        def wrapper(*args, **kwargs):
+            if mode == RECORD:
+                callback(*args, **kwargs)
+                callback_mocker.save_callback_signatures()
+            elif mode == REPLAY:
+                callback_mocker.eval_callback_signatures(callback_output_name, callback_output)
+                callback(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
---
+++ test_keras.py
@@ -0,0 +1,92 @@
+from capturemock import capturemock, RECORD, REPLAY
+from callback_mocker import CallbackMocker, callbackmock, callbackmocker
+
+
+callback_mocker = CallbackMocker()
+callback_output = []
+mode = REPLAY
+
+
+class Model:
+    def __init__(self):
+        from tensorflow.keras.models import Sequential
+        from tensorflow.keras.layers import Dense
+
+        self.num_classes = 10
+        self.input_shape = (1, 784)
+
+        self.model = Sequential()
+        self.model.add(Dense(800, input_dim=self.input_shape[1], activation='relu'))
+        self.model.add(Dense(100, activation='relu'))
+        self.model.add(Dense(self.num_classes, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    def get_dataset(self):
+        from tensorflow.keras.datasets import mnist
+        from tensorflow.keras import utils
+
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        x_train = x_train.reshape(x_train.shape[0], -1)
+        x_train = x_train.astype('float32') / 255.
+        y_train = utils.to_categorical(y_train, self.num_classes)
+
+        return x_train, y_train
+
+    def fit(self):
+        from tensorflow.keras.callbacks import Callback
+
+        class FitCallback(Callback):
+            @callbackmock(callback_mocker, 'callback_output')
+            def on_train_end(self, logs=None):
+                print(logs)
+                return logs
+
+            @callbackmock(callback_mocker, 'callback_output')
+            def on_epoch_end(self, epoch, logs=None):
+                print(logs)
+                return logs
+
+        x_train, y_train = self.get_dataset()
+        history = self.model.fit(x_train,
+                                 y_train,
+                                 callbacks=[FitCallback()],
+                                 batch_size=128,
+                                 epochs=15,
+                                 verbose=1)
+
+        history = history.__dict__
+        history['model'] = None
+
+        return history
+
+
+def general_fit_function():
+    model = Model()
+    history = model.fit()
+
+    return history
+
+
+@callbackmocker(callback_mocker, 'callback_output', callback_output, mode)
+@capturemock('__main__.general_fit_function', mode=mode)
+def test_training():
+    history = general_fit_function()
+
+    metrics = list(history['history'].keys())
+    params = list(history['params'].values())
+    on_train_end = callback_output[-1]
+
+    print(callback_output)
+    print(metrics)
+    print(params)
+
+    # for some reason capturemock serialization\deserialization process goes other way
+    # and dict keys swap places in REPLAY and RECORD modes
+    assert 'loss' in metrics and 'accuracy' in metrics
+    assert 1 in params and 15 in params and 469 in params
+    assert 'loss' in on_train_end and 'accuracy' in on_train_end
+
+
+if __name__ == '__main__':
+    test_training()
